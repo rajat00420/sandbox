90b732a36866faf4ebccbaf038742c0645344131
jc441938 <rajat.khajuria@my.jcu.edu.au>
2016-12-09 20:04:29 +1000

    complete file

--- oddName.py
+++ oddName.py
@@ -1,1 +1,12 @@
-__author__ = 'jc441938'
+
+valid = False
+while not valid:
+    name = input("enter your name")
+    if name != "" and not name.isspace():
+        valid = True
+print(valid)
+
+
+l=len(name)
+for i in range(0,l,2):
+    print(name[i])
